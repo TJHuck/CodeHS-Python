11817c5f575c0d14a14700e87381fda8876fb9e0
TJHuck <78052758+TJHuck@users.noreply.github.com>
2022-05-16 11:54:27 -0400

    Calls for the length of the word in the for loop
    Calls for the length of the word in the for loop, allowing it to work with other words automatically by changing the repetition in the for loop to x for the amount of letters in the word.

--- Part1,ReplaceaLetter.py
+++ Part1,ReplaceaLetter.py
@@ -1,14 +1,10 @@
 def replace_at_index(old, index):
-    
     string = list(old)
-    
     string[index] = "-";
-    
     returnString = ""
-    
-    for i in range(8):
+    x = len(old)
+    for i in range(x):
         returnString = returnString + string[i]
-    
     return returnString
         
-print replace_at_index("eggplant", 3)
+print(replace_at_index("eggplant", 3))
